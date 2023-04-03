f250f689c021f21be2c80e6a42d5d91768a9ff46
AsuKakki <hgs98927@gmail.com>
2023-04-02 22:09:44 -0400

    Code for define database and data visualization

---
+++ Database&Visualization/citydata.sql
@@ -0,0 +1,40 @@
+create database citydata;
+use citydata;
+
+create table atlantaoutput(
+	X double,
+	Y double,
+	Type varchar(20),
+	Hotel varchar(1),
+	Name varchar(100),
+    cluster varchar(10)
+);
+
+create table orlandooutput(
+	X double,
+	Y double,
+	Type varchar(20),
+	Hotel varchar(1),
+	Name varchar(100),
+    cluster varchar(10)
+);
+
+create table sanfranciscooutput(
+	X double,
+	Y double,
+	Type varchar(20),
+	Hotel varchar(1),
+	Name varchar(100),
+    cluster varchar(10)
+);
+
+create table bostonoutput(
+	X double,
+	Y double,
+	Type varchar(20),
+	Hotel varchar(1),
+	Name varchar(100),
+    cluster varchar(10)
+);
+
+
